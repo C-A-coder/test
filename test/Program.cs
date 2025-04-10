2fc75b08556b6e3c129d9adc3d7c7c4e23fcfef3
Clemens Ausserer <clemens.ausserer@hak-feldkirch.at>
2025-04-10 19:23:45 +0200

    Dies ist ein erster Versuch.

--- test/Program.cs
+++ test/Program.cs
@@ -1,6 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-
-// kommentar
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Test");
+    }
+}
